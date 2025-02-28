b6069d30369bfbbb591b5da6a540cb135c986aa5
Uqbah951 <113536021+Uqbah951@users.noreply.github.com>
2025-02-28 11:05:58 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
+  packname: 'ukhbah',
   author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "Ukhbah Bot",
+  botOwner: 'Uqbah', // Your name
+  ownerNumber: '9075071116', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
